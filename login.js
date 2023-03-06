0393637379182bfcc48194af5be2906002f66a5b
tryentering <toby0jackson@gmail.com>
2023-03-06 16:26:40 -0700

    Actual final commit (removing test data)

--- login.js
+++ login.js
@@ -2,6 +2,5 @@ function login() {
     const nameFromInput = document.querySelector("#name");
     localStorage.setItem("userName", nameFromInput.value);
     window.location.href = "play.html";
-    localStorage.setItem("scores", JSON.stringify([{name: "TEST", score: 9, date: '9/9/9999'}]));
     //add name from the input to local storage
 }
